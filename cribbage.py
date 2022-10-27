3a67b10936d8ac769b0675566ddb302fd48a3479
cathy-zheng <zheng465@umn.edu>
2022-10-26 21:43:40 -0500

    greet user and initiate scores

---
+++ cribbage.py
@@ -0,0 +1,13 @@
+# Greet User
+user_name = input("Hello, what's your name? ")
+print(f"Welcome, {user_name}. Let's play cribbage!")
+
+# Start of a new game, computer and user both start at 0 points.
+cpu_score=0
+user_score=0
+score_tracker={}
+
+# Draw for first crib, lower goes
+# Deal cards
+
+
